e4ac87ea201a0a007401f11b4249fb3d2192b757
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-02 13:02:58 +0530

    day 2 leetcode Happy Number

---
+++ talkinsidejava.io/src/main/java/com/a30day/leetcode/HappyNumber.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package com.a30day.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author nisingh
+ *
+ *	Write an algorithm to determine if a number is "happy".
+
+	A happy number is a number defined by the following process: Starting with any positive integer,
+	 replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay),
+	  or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+	
+	Example: 
+	
+	Input: 19
+	Output: true
+	Explanation: 
+	12 + 92 = 82
+	82 + 22 = 68
+	62 + 82 = 100
+	12 + 02 + 02 = 1
+ *
+ */
+public class HappyNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int n =2;
+		
+		System.out.println(findHappyNumber(n));
+	}
+
+	private static boolean findHappyNumber(int n) {
+		List<Integer> ls = new ArrayList<Integer>();
+		while (n>=1) {
+			int res = findSquareandSum(n);
+			System.out.println("res: "+ res);
+			if(res==1) {
+				return true;
+			}else if(ls.contains(res)){
+				return false;
+			}else {
+				ls.add(res);
+				n = res;
+			};
+		}
+		
+		
+		return false;
+	}
+
+	private static int findSquareandSum(int n) {
+		int res = 0;
+		while(n>0) {
+			int r = n%10;
+			res += (r*r);
+			n = n/10;		
+		}
+		return res;
+	}
+
+}
