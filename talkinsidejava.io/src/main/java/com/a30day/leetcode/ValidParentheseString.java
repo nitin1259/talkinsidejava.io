d9d672e56dd7ad19ce73bd16fe195c2bb42a822c
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-27 23:30:37 +0530

    todo valid Parethese string

---
+++ talkinsidejava.io/src/main/java/com/a30day/leetcode/ValidParentheseString.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.a30day.leetcode;
+
+/**
+ * @author nisingh
+ * 
+ *         Given a string containing only three types of characters: '(', ')'
+ *         and '*', write a function to check whether this string is valid. We
+ *         define the validity of a string by these rules:
+ * 
+ *         Any left parenthesis '(' must have a corresponding right parenthesis
+ *         ')'. Any right parenthesis ')' must have a corresponding left
+ *         parenthesis '('. Left parenthesis '(' must go before the
+ *         corresponding right parenthesis ')'. '*' could be treated as a single
+ *         right parenthesis ')' or a single left parenthesis '(' or an empty
+ *         string. An empty string is also valid. Example 1: Input: "()" Output:
+ *         True Example 2: Input: "(*)" Output: True Example 3: Input: "(*))"
+ *         Output: True Note: The string size will be in the range [1, 100].
+ *
+ */
+public class ValidParentheseString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
